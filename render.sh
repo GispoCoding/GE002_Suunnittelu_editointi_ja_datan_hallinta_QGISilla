136ba0f67f6f8e1ed6a46f6b70be21a8dfa69309
Juho Ervasti <118284595+JuhoErvasti@users.noreply.github.com>
2023-07-03 11:03:23 +0300

    Update render.sh

--- render.sh
+++ render.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
-
-docker run --rm -v "$(pwd)/src:/app" -v "$(pwd)/out:/out" gispo/bookdown:latest /app/run.sh
+docker run -it --rm -v "${pwd}/src:/app".ToLower() -v "${pwd}/out:/out".ToLower() gispo/bookdown:latest bash -c "set -e \
+rm -rf /out/* \
+Rscript -e \`"bookdown::render_book('/app', 'bookdown::html_book')\`" \
+mv /app/_book/* /out/ && rm -r /app/_book && chmod --recursive 777 /out"
